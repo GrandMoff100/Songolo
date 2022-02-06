ca969eab110b044f7a2503511fba727ea18c6a5e
GrandMoff100 <minecraftcrusher100@gmail.com>
2022-02-06 05:06:27 +0000

    Linted the codebase

--- songolo/songs.py
+++ songolo/songs.py
@@ -1,22 +1,26 @@
+import hashlib
 import json
 import os
-import hashlib
-import youtube_dl
+from typing import Dict, Optional
+
+import eyed3  # type: ignore[import]
+import youtube_dl  # type: ignore[import]
 from pydantic import BaseModel
-from typing import Optional, Dict
-from pygit2 import Repository
+from pygit2 import Repository  # type: ignore[import]
 
 
 class Library(BaseModel):
+    path: str = None
     prefix = "[Songolo] "
 
-    def __init__(self, path: Optional[str] = None) -> None:
-        if path is None:
-            path = ".songolo"
-            os.mkdir(path)
-        if not os.path.exists(path):
-            raise OSError(f"Directory {path!r} does not exist.")
-        self.repo = Repository(path)
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        if self.path is None:
+            self.path = ".songolo"
+            os.mkdir(self.path)
+        if not os.path.exists(self.path):
+            raise OSError(f"Directory {self.path!r} does not exist.")
+        self.repo = Repository(self.path)
 
     def cleanse_working_tree(self):
         pass
@@ -32,15 +36,16 @@ class Song(BaseModel):
     prefix: str = "mp3"
     extras: Dict[str, str] = {}
     content: Optional[bytes] = None
+    library: Library = Library()
 
     def digest(self) -> str:
-        text = f"{author} {title} {link}"
+        text = f"{self.author} {self.title} {self.link}"
         return hashlib.sha256(text.encode()).hexdigest()
 
-    def save(self, library: Library):
+    def save(self):
         if self.content:
-            path = os.path.join(library.path, self.digest() + "." + self.prefix)
-            with open(path, "w") as f:
+            path = os.path.join(self.library.path, f"{self.digest()}.{self.prefix}")
+            with open(path, "wb") as f:
                 f.write(self.content)
             meta = eyed3.load(path)
             meta.tag.artist = self.author
@@ -49,14 +54,9 @@ class Song(BaseModel):
             meta.tag.extras = json.dumps(self.extras)
             meta.tag.save()
 
-    def download(self, library: Library):
-        options = self._options(library)
-        with youtube_dl.YoutubeDL() as ydl:
+    def download(self):
+        with youtube_dl.YoutubeDL(self.options) as ydl:
             ydl.download([self.link])
 
-    def _options(self, library: Library):
+    def _options(self):
         return {}
-
-
-
-
