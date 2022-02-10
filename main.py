944bb8b70e7773ac9a3b78fa24d452ce33ad2af4
GrandMoff100 <minecraftcrusher100@gmail.com>
2022-02-10 01:34:20 +0000

    Add lib and snowflake in metadata

--- main.py
+++ main.py
@@ -1,39 +1,56 @@
 from songolo.songs import Library, MetaData, Song
+from songolo.utils import sha256_snowflake
 
-lib = Library()
+
+lib = Library(path="dist")
 
 
 songs = [
     Song(
+        library=lib,
+        snowflake=sha256_snowflake(),
         meta=MetaData(
             artist="One Republic",
             title="Secrets",
-            link="https://www.youtube.com/watch?v=7U-9NlSmNKc",
+            extras=dict(
+                link="https://www.youtube.com/watch?v=7U-9NlSmNKc",
+                snowflake=sha256_snowflake(),
+            ),
         )
     ),
     Song(
+        library=lib,
+        snowflake=sha256_snowflake(),
         meta=MetaData(
+            extras=dict(
+                snowflake=sha256_snowflake(),
+                link="https://www.youtube.com/watch?v=fHI8X4OXluQ",
+            ),
             artist="The Weeknd",
             title="Blinding Lights",
-            link="https://www.youtube.com/watch?v=fHI8X4OXluQ",
+            
         )
     ),
     Song(
+        library=lib,
         meta=MetaData(
+            extras=dict(
+                snowflake=sha256_snowflake(),
+                link="https://open.spotify.com/track/6kcHg7XL6SKyPNd78daRBL?si=612bf861a2364a6b",
+            ),
             artist="Saint Motel",
             title="My Type",
-            link="https://open.spotify.com/track/6kcHg7XL6SKyPNd78daRBL?si=612bf861a2364a6b",
         )
     ),
 ]
 
 
-for song in songs:
-    if "youtube" in song.meta.link:
-        song.download("youtube")
-    elif "spotify" in song.meta.link:
-        song.download("spotify")
+def main():
+    for song in songs:
+        if song.meta.link is not None:
+            if "youtube" in song.meta.link:
+                song.download("youtube")
+            elif "spotify" in song.meta.link:
+                song.download("spotify")
 
-
-for song in lib.songs():
-    print(song)
+main()
